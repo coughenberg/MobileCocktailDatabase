7b5e37f3a68aa54f24333b59ec90bbed5d47373f
coughenberg <khfantk@gmail.com>
2020-10-10 17:59:45 -0500

    GET request for the list of ingredients user can submit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:MobileCocktailDatabase/main.dart';
+import 'package:new_flutter_project_prt2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
