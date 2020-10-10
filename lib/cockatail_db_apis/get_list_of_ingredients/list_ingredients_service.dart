e8255cdf9483aecd21010cc527dd11a2f20e6100
coughenberg <khfantk@gmail.com>
2020-10-10 18:36:42 -0500

    Update on apiKey Retrieval

--- lib/cockatail_db_apis/get_list_of_ingredients/list_ingredients_service.dart
+++ lib/cockatail_db_apis/get_list_of_ingredients/list_ingredients_service.dart
@@ -1,21 +1,31 @@
 import 'dart:convert';
 
-import 'package:MobileCocktailDatabase/cockatail_db_apis/base_cocktaildb_info.dart';
 import 'package:http/http.dart' as http;
-import 'list_ingredients_interface.dart';
+
+import 'package:MobileCocktailDatabase/cockatail_db_apis/base_cocktaildb_info.dart';
+import 'package:MobileCocktailDatabase/cockatail_db_apis/get_list_of_ingredients/list_ingredients_service.dart';
+import 'package:MobileCocktailDatabase/cockatail_db_apis/get_list_of_ingredients/list_ingredients_interface.dart';
+import 'package:MobileCocktailDatabase/cockatail_db_apis/apiKey.dart';
+
+void main() {
+  IngredientList().getIngredientsListCall();
+}
 
 class IngredientList {
   // main is only here right now for debugging purposes, uncomment when testing backend service
-  // void main() {
-  //   getIngredientsListCall();
-  // }
+  void main() {
+    getIngredientsListCall();
+  }
 
   ///The call to get the list of ingredients
   ///
   ///@returns Future<List<Ingredient>> from the class list_ingredients_interface
   Future<List<Ingredient>> getIngredientsListCall() async {
+    print('hello');
     var url = '$BASE_URL/$VERSION/$API_KEY/list.php?i=list';
+    print(url);
     var responseString = await http.get(url);
+    print('hello');
     Map<String, dynamic> response = jsonDecode(responseString.body);
     return IngredientResponse.fromJson(response).ingredients;
   }
